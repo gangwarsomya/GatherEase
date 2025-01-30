493c588b39f55bc6b65d179fa082c0971bac4a2a
gangwarsomya <gangwarsomya16@gmail.com>
2025-01-31 00:38:43 +0530

    chnges

---
+++ components/shared/EventForm.tsx
@@ -0,0 +1,283 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { eventFormSchema } from "@/lib/validator";
+import * as z from "zod";
+import { eventDefaultValues } from "@/constants";
+import Dropdown from "./Dropdown";
+import { Textarea } from "@/components/ui/textarea";
+import { useState } from "react";
+import Image from "next/image";
+import DatePicker from "react-datepicker";
+import { UploadButton, UploadDropzone } from "@/lib/uploadthing";
+import "react-datepicker/dist/react-datepicker.css";
+import { Checkbox } from "../ui/checkbox";
+import { useRouter } from "next/navigation";
+import { createEvent, updateEvent } from "@/lib/actions/event.actions";
+import { IEvent } from "@/lib/database/models/event.model";
+
+type EventFormProps = {
+  userId: string;
+  type: "Create" | "Update";
+  event?: IEvent;
+  eventId?: string;
+};
+
+const EventForm = ({ userId, type, event, eventId }: EventFormProps) => {
+  const [files, setFiles] = useState<File[]>([]);
+  const [uploadedImageUrl, setUploadedImageUrl] = useState("");
+  const router = useRouter();
+
+  const initialValues =
+    event && type === "Update"
+      ? { ...event, startDateTime: new Date(event.startDateTime), endDateTime: new Date(event.endDateTime) }
+      : eventDefaultValues;
+
+  const form = useForm<z.infer<typeof eventFormSchema>>({
+    resolver: zodResolver(eventFormSchema),
+    defaultValues: initialValues,
+  });
+
+  async function onSubmit(values: z.infer<typeof eventFormSchema>) {
+    if (!uploadedImageUrl && files.length > 0) {
+      return;
+    }
+
+    const imageUrl = uploadedImageUrl || values.imageUrl;
+
+    if (type === "Create") {
+      try {
+        const newEvent = await createEvent({
+          event: { ...values, imageUrl },
+          userId,
+          path: "/profile",
+        });
+
+        if (newEvent) {
+          form.reset();
+          router.push(`/events/${newEvent._id}`);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    if (type === "Update" && eventId) {
+      try {
+        const updatedEvent = await updateEvent({
+          userId,
+          event: { ...values, imageUrl, _id: eventId },
+          path: `/events/${eventId}`,
+        });
+
+        if (updatedEvent) {
+          form.reset();
+          router.push(`/events/${updatedEvent._id}`);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5">
+        <div className="flex flex-col gap-5 md:flex-row">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormLabel>Event Title</FormLabel>
+                <FormControl>
+                  <Input placeholder="Event title" {...field} className="input-field" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="categoryId"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormLabel>Category</FormLabel>
+                <FormControl>
+                  <Dropdown onChangeHandler={field.onChange} value={field.value} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <div className="flex flex-col gap-5 md:flex-row">
+          <FormField
+            control={form.control}
+            name="description"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormLabel>Description</FormLabel>
+                <FormControl>
+                  <Textarea placeholder="Event description" {...field} className="textarea rounded-2xl" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <div className="flex flex-col gap-5 md:flex-row">
+          <FormField
+            control={form.control}
+            name="location"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormLabel>Location</FormLabel>
+                <FormControl>
+                  <Input placeholder="Event location" {...field} className="input-field" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <div className="flex flex-col gap-5 md:flex-row">
+          <FormField
+            control={form.control}
+            name="startDateTime"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormLabel>Start Date</FormLabel>
+                <FormControl>
+                  <DatePicker
+                    selected={field.value}
+                    onChange={(date: Date) => field.onChange(date)}
+                    showTimeSelect
+                    timeInputLabel="Time:"
+                    dateFormat="MM/dd/yyyy h:mm aa"
+                    wrapperClassName="w-full"
+                    className="input-field"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="endDateTime"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormLabel>End Date</FormLabel>
+                <FormControl>
+                  <DatePicker
+                    selected={field.value}
+                    onChange={(date: Date) => field.onChange(date)}
+                    showTimeSelect
+                    timeInputLabel="Time:"
+                    dateFormat="MM/dd/yyyy h:mm aa"
+                    wrapperClassName="w-full"
+                    className="input-field"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <div className="flex flex-col gap-5 md:flex-row">
+          <FormField
+            control={form.control}
+            name="price"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormLabel>Price</FormLabel>
+                <FormControl>
+                  <Input 
+                    type="number" 
+                    placeholder="Price" 
+                    {...field} 
+                    className="input-field"
+                    onChange={(e) => field.onChange(e.target.valueAsNumber || 0)}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="isFree"
+            render={({ field }) => (
+              <FormItem className="flex w-full items-start space-x-3 space-y-0 rounded-md border p-4">
+                <FormControl>
+                  <Checkbox
+                    checked={field.value}
+                    onCheckedChange={field.onChange}
+                  />
+                </FormControl>
+                <div className="space-y-1 leading-none">
+                  <FormLabel>Free Event</FormLabel>
+                </div>
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <div className="flex flex-col gap-5 md:flex-row">
+          <FormField
+            control={form.control}
+            name="url"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormLabel>URL</FormLabel>
+                <FormControl>
+                  <Input placeholder="Event URL" {...field} className="input-field" />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <FormField
+          control={form.control}
+          name="imageUrl"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Image</FormLabel>
+              <FormControl>
+                <UploadDropzone
+                  endpoint="imageUploader"
+                  onClientUploadComplete={(res) => {
+                    if (res && res[0]?.url) {
+                      setUploadedImageUrl(res[0].url);
+                      field.onChange(res[0].url);
+                    }
+                  }}
+                  onUploadError={(error) => console.error(error)}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <Button type="submit" size="lg" disabled={form.formState.isSubmitting} className="button col-span-2 w-full">
+          {form.formState.isSubmitting ? "Submitting..." : `${type} Event`}
+        </Button>
+      </form>
+    </Form>
+  );
+};
+
+export default EventForm;
